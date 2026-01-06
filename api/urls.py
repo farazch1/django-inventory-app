eda86ae0de782c855d4fd5457e2a2648cc14f726
Mian Faraz <mianfaraz2002@gmail.com>
2026-01-06 21:07:36 +0500

    update

--- api/urls.py
+++ api/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from django.conf.urls.static import static
-from django.conf import settings
 from django.views.generic import RedirectView
 
 urlpatterns = [
