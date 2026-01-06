bf911663bbfc6ec79431a52f6092064375244488
Mian Faraz <mianfaraz2002@gmail.com>
2026-01-06 21:49:54 +0500

    update

--- api/settings.py
+++ api/settings.py
@@ -83,10 +83,16 @@ WSGI_APPLICATION = 'api.wsgi.app'
 
 # DATABASES = {}
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
